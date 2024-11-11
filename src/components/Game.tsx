0da75d7f949042158f3c838de255134aedbd676d
CorentinCLERO <corentin.clero@epsi.fr>
2024-11-11 23:22:44 +0100

    📦 NEW: game working with scorboard

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,39 +2,46 @@ import React, { useCallback, useEffect, useState } from "react";
 import { Input } from "@/components/ui/input";
 import getTextScore from "@/app/utils/getTextScore";
 import { SongType } from "@/app/rapGame/[id]/page";
-import { Badge } from "./ui/badge";
 
 type followScoreType = {
   score: number;
   step: number;
   goodAnswers: number;
+  askedRhymes: string;
 };
 
 function Game(getSong: SongType) {
   const [rapText, setRapText] = useState<string>("");
-  const [nextbuttonVisible, setNextbuttonVisible] = useState<boolean>(false);
   const [isValid, setIsValid] = useState<boolean | null>(null);
+  const localScoreString = localStorage.getItem(`${getSong.id}`);
+  const localScore = localScoreString ? JSON.parse(localScoreString) : null;
   const [scores, setScores] = useState<followScoreType>({
-    score: 0,
-    step: 0,
-    goodAnswers: 0,
+    score: localScore ? localScore.score : 0,
+    step: localScore ? localScore.step : 0,
+    goodAnswers: localScore ? localScore.goodAnswers : 0,
+    askedRhymes: localScore ? localScore.askedRhymes : "",
   });
+  const [nextbuttonVisible, setNextbuttonVisible] = useState<boolean>(localScore ? localScore.step === 10 : false);
   const [RandomNumber, setRandomNumber] = useState<number | null>(null);
   const song = React.useMemo(
     () => (RandomNumber ? getSong.lyrics[RandomNumber] : "Loading..."),
     [getSong.lyrics, RandomNumber]
   );
+  const validatedScore = localStorage.getItem(`score-${getSong.id}`);
+  const validatedScoresArray = validatedScore ? validatedScore.split(',') : []
 
   const getRandomNumber = useCallback(() => {
-    setRandomNumber(Math.floor(Math.random() * getSong.lyrics.length));
-  }, [getSong.lyrics.length]);
+    const randomNumberGenerate = Math.floor(Math.random() * getSong.lyrics.length)
+    if (scores.askedRhymes.includes(randomNumberGenerate.toString())) getRandomNumber()
+    setRandomNumber(randomNumberGenerate);
+  }, [getSong.lyrics.length, scores.askedRhymes]);
 
   useEffect(() => {
     if (getSong.lyrics) getRandomNumber()
   }, [getSong.lyrics, getRandomNumber]);
 
   const validateScore = () => {
-    if (RandomNumber === null) return;
+    if (RandomNumber === null || rapText.length <=5) return;
 
     const result = getTextScore(rapText, getSong.lyrics[RandomNumber + 1]);
     setIsValid(result.isValid);
@@ -42,6 +49,7 @@ function Game(getSong: SongType) {
       score: scores.score + result.value,
       step: scores.step + 1,
       goodAnswers: result.isValid ? scores.goodAnswers + 1 : scores.goodAnswers,
+      askedRhymes: scores.askedRhymes ? `${scores.askedRhymes},${RandomNumber}` : `${RandomNumber}`
     }
     setScores(scoresActual)
     localStorage.setItem(`${getSong.id}`, JSON.stringify(scoresActual))
@@ -51,10 +59,28 @@ function Game(getSong: SongType) {
   const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
     if (e.key === "Enter" && !nextbuttonVisible) {
       validateScore();
+    } else if (e.key === "Enter" && nextbuttonVisible) {
+      nextQuestion();
     }
   };
 
   const nextQuestion = () => {
+    setRapText("");
+    setIsValid(null);
+    setNextbuttonVisible(false);
+    getRandomNumber();
+  };
+
+  const resetData = () => {
+    const pourcentageGoodAnswers = Math.round(scores.score * 1000) / 100
+    localStorage.setItem(`score-${getSong.id}`, validatedScore ? `${validatedScore},${scores.goodAnswers}/${pourcentageGoodAnswers}/${Date.now()}` : `${scores.goodAnswers}/${pourcentageGoodAnswers}/${Date.now()}` );
+    localStorage.removeItem(`${getSong.id}`);
+    setScores({
+      score: 0,
+      step: 0,
+      goodAnswers: 0,
+      askedRhymes: "",
+    });
     setRapText("");
     setIsValid(null);
     setNextbuttonVisible(false);
@@ -66,7 +92,7 @@ function Game(getSong: SongType) {
   return (
     <div className="flex justify-center">
       <div className="flex flex-col gap-5 w-4/5 mt-10">
-        <div className="text-center text-xl">{song}</div>
+        <div className="text-center text-xl">{scores.step === 10 ? "Tu as finis !" : song}</div>
         <Input
           type="text"
           onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
@@ -78,6 +104,7 @@ function Game(getSong: SongType) {
             isValid === null ? "" : isValid ? "text-green-500" : "text-red-500"
           }`}
           onKeyDown={handleKeyDown}
+          disabled={scores.step === 10}
         />
         <div className="flex justify-between">
           <button
@@ -87,16 +114,49 @@ function Game(getSong: SongType) {
             >
             Valider
           </button>
-          <button
+          {scores.step !== 10 && <button
             className="border-none border-2 rounded-full px-5 py-2 text-xl bg-stone-500 hover:bg-stone-700 text-white disabled:bg-slate-300"
             onClick={() => nextQuestion()}
             disabled={!nextbuttonVisible}
           >
             Suivant
-          </button>
+          </button>}
         </div>
         <div className="flex justify-end mt-20">
-          <div className="border-none border-2 rounded-full px-5 py-2 text-base bg-stone-500 text-white font-bold">{scores.step}/10</div>
+          <div className="border-none border-2 rounded-full px-5 py-2 text-base bg-stone-700 text-white font-bold hover:cursor-default">{scores.step}/10</div>
+        </div>
+        {scores.step === 10 && 
+          <div>
+            <button
+              className="border-none border-2 rounded-full px-5 py-2 text-xl bg-stone-500 hover:bg-stone-700 text-white"
+              onClick={resetData}
+            >
+              Recommencer
+            </button>
+          </div>
+        }
+        <div>
+          <div className="mb-5">Tableau des scores :</div>
+          <div className="grid grid-cols-3 border">
+            <div className="border p-2">Date</div>
+            <div className="border p-2">Scores /10</div>
+            <div className="border p-2">% d&apos;éxactitude</div>
+            <div className="border p-2">En cours</div>
+            <div className="border p-2">{scores.goodAnswers}</div>
+            <div className="border p-2">{Math.round(scores.score * 1000) / 100}</div>
+            {
+              validatedScoresArray.map((score, index) => {
+                const scoreDetails = score.split('/')
+                return (
+                  <React.Fragment key={index}>
+                    <div className="border p-2">{new Date(Number(scoreDetails[2])).toLocaleString()}</div>
+                    <div className="border p-2">{scoreDetails[0]}</div>
+                    <div className="border p-2">{scoreDetails[1]}</div>
+                  </React.Fragment>
+                );
+              })
+            }
+          </div>
         </div>
       </div>
     </div>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -6,6 +6,8 @@ import { useRouter, useSearchParams } from 'next/navigation';
 import { useCallback } from 'react';
 import debounce from 'lodash.debounce';
 import Link from 'next/link';
+// import logoSvg from '@/assets/logo.svg';
+import Image from 'next/image';
 
 interface HeaderProps {
   displaySearchBar?: boolean;
@@ -47,9 +49,16 @@ function Header({ displaySearchBar = false, song }: HeaderProps) {
   return (
     <div className='border-b'>
       <div className='flex h-20 ms-5 me-10 justify-between'>
-        <Link href="/" className='content-center text-3xl'>
-          Rhymes
-        </Link>
+          <Link href="/" className='flex'>
+            <Image
+              src="/logo.svg"
+              alt="Logo"
+              width={40}
+              height={40}
+              className='content-center'
+            />          
+            <div className='content-center ms-5 text-3xl'>Rhymes</div>
+          </Link>
         {displaySearchBar && <div className='content-center rounded-lg text-black'>
           <Input type="text" placeholder="Rechercher..." onChange={handleInputChange} value={inputValue} />
         </div>}
